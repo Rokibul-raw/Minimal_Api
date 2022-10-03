052e87e099c0df3f9b8047a33afc7b4b30c8bd9f
Rokibul Islam <mymainac2022@gmail.com>
2022-10-03 20:49:28 +0600

    post put and delete operation

--- Data/VistorDbContext.cs
+++ Data/VistorDbContext.cs
@@ -14,3 +14,6 @@ namespace MinimalApi.Data
 
     }
 }
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Data;
+using MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<VistorDbContext>(options =>
@@ -42,7 +43,46 @@ app.MapGet("/Get_All_Data", async (VistorDbContext context) =>
     return Results.Ok(get_all_data);
 
 });
-    
+//Post Data
+app.MapPost("/Post_Data",async (Visitor_Info visitor_Info ,VistorDbContext context) =>
+{
+    context.VisitorInfos.Add(visitor_Info);
+    await context.SaveChangesAsync();
+    return Results.Ok();
+
+}).WithName("Post_User");
+app.MapPut("/Update_Data", async(Visitor_Info visitor_Info, VistorDbContext context) =>
+{
+    var update_data = await context.VisitorInfos.FindAsync(visitor_Info.Id);
+    if(update_data != null)
+    {
+        update_data.FullName = visitor_Info.FullName;
+        update_data.Mob_No=visitor_Info.Mob_No;
+        update_data.Address = visitor_Info.Address;
+        await context.SaveChangesAsync();
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+}).WithName("Update Data");
+
+app.MapDelete("/Delete_Data", async (int id, VistorDbContext context) =>
+{
+    var delete_data=await context.VisitorInfos.FindAsync(id);
+    if(delete_data != null)
+    {
+        context.VisitorInfos.Remove(delete_data);
+        await context.SaveChangesAsync();
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.NoContent();
+    }
+
+}).WithName("Delete Data");
 
 app.Run();
 
